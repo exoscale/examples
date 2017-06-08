32499292c57a3628ac11b8318ae4ec7245c09079
Sanja Bonic <thoughtkettle@gmail.com>
2017-06-08 09:18:33 +0200

    Format Go.

--- one-package-web-server/src/main.go
+++ one-package-web-server/src/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-  "html/template"
-  "net/http"
+	"html/template"
+	"net/http"
 	"os"
 )
 
 type MultipleDomains map[string]http.Handler
 
 func (md MultipleDomains) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  handler := md[r.Host]
+	handler := md[r.Host]
 
-  if handler != nil {
-    handler.ServeHTTP(w, r)
-  } else {
-    http.Redirect(w, r, "/", http.StatusFound)
-  }
+	if handler != nil {
+		handler.ServeHTTP(w, r)
+	} else {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
 }
 
 // templateHandler assigns each URL to the corresponding template structure.
 func templateHandler(w http.ResponseWriter, r *http.Request) {
-  layout := "../structure.html"
+	layout := "../structure.html"
 	content := "../pages/" + r.URL.Path + ".html"
-	if r.URL.Path == "/" {content = "../pages/index.html"}
+	if r.URL.Path == "/" {
+		content = "../pages/index.html"
+	}
 
 	info, err := os.Stat(content)
 	if err != nil {
@@ -47,14 +49,14 @@ func templateHandler(w http.ResponseWriter, r *http.Request) {
 
 // main starts the web server and routes URLS.
 func main() {
-  muxex := http.NewServeMux()
+	muxex := http.NewServeMux()
 
-  md := make(MultipleDomains)
-  md["localhost:8080"] = muxex
+	md := make(MultipleDomains)
+	md["localhost:8080"] = muxex
 
-  muxex.Handle("/public/images/", http.StripPrefix("/public/images/", http.FileServer(http.Dir("../public/images"))))
-  muxex.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("../public"))))
-  muxex.HandleFunc("/", templateHandler)
+	muxex.Handle("/public/images/", http.StripPrefix("/public/images/", http.FileServer(http.Dir("../public/images"))))
+	muxex.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("../public"))))
+	muxex.HandleFunc("/", templateHandler)
 
-  http.ListenAndServe(":8080", md)
+	http.ListenAndServe(":8080", md)
 }
